947c97b45a3bfdc287656324d0bf32973f816766
dheerajeshwar32 <nagula.deeraj2024@vitstudent.ac.in>
2026-09-20 18:32:15 +0530

    chore: remove nested duplicate repo and Vite template assets

--- PerfectByte/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ["@jsquash/jpeg", "@jsquash/png", "@jsquash/webp", "@jsquash/avif"]
-  }
-})
